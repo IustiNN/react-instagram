a3abc989cbc5cf62a61bbc5d07ca7d26237bcc7a
iustin <iustin.nitza@gmail.com>
2016-10-19 22:10:06 +0300

    Add main components

---
+++ src/components/Grid.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Grid extends Component {
+    render() {
+        return (
+            <p>Grid</p>
+        );
+    }
+}
+
+export default Grid;
---
+++ src/components/Single.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Single extends Component {
+    render() {
+        return (
+            <p>Single</p>
+        );
+    }
+}
+
+export default Single;
