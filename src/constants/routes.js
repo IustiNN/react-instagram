2a72980dcdaccfd4814a8cf577e68b73f2f3aaf3
iustin <iustin.nitza@gmail.com>
2018-04-06 01:41:07 +0200

    Create routes constants

---
+++ src/constants/routes.js
@@ -0,0 +1,6 @@
+export const SIGN_UP = '/signup';
+export const SIGN_IN = '/signin';
+export const LANDING = '/';
+export const FEED = '/feed';
+export const ACCOUNT = '/account';
+export const FORGOT_PASSWORD = '/forgot-password';
