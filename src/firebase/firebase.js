23917c89dd3b2cfb9674fafad93eff0c514e5d44
iustin <iustin.nitza@gmail.com>
2018-04-06 11:59:26 +0200

    add firebase config

---
+++ src/firebase/firebase.js
@@ -0,0 +1,17 @@
+import * as firebase from 'firebase';
+var config = {
+    apiKey: "AIzaSyDQqozvfmcbEx6HCU-ZrgTxXqRdMkTapd0",
+    authDomain: "react-instagram-d11f7.firebaseapp.com",
+    databaseURL: "https://react-instagram-d11f7.firebaseio.com",
+    projectId: "react-instagram-d11f7",
+    storageBucket: "react-instagram-d11f7.appspot.com",
+    messagingSenderId: "907217772181"
+};
+
+if (!firebase.apps.length) {
+    firebase.initializeApp(config);
+}
+const auth = firebase.auth();
+export {
+    auth,
+};
