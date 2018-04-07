1c4dbc7e94fc7415ab904ad7e8b1a3412432bb20
iustin <iustin.nitza@gmail.com>
2018-04-07 14:19:37 +0200

    Add different navigation based on auth

--- src/components/common/Navigation.js
+++ src/components/common/Navigation.js
@@ -2,14 +2,31 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import SingOutButton from '../user/SignOut';
 import * as routes from '../../constants/routes';
-const Navigation = () =>
+const Navigation = ({authUser}) =>
+    <div>
+        {
+            {authUser} 
+            ? <NavigationAuth/>
+            : <NavigationDefault/>
+        }
+    </div>
+
+const NavigationAuth = () =>
     <div>
         <ul>
             <li><Link to={routes.SIGN_IN}>Sign In</Link></li>
             <li><Link to={routes.LANDING}>Landing</Link></li>
             <li><Link to={routes.FEED}>Feed</Link></li>
-            <li><Link to={routes.ACCOUNT}>Account</Link></li>
-            <li><SingOutButton/></li>
+            <li><SingOutButton /></li>
         </ul>
     </div>
+
+const NavigationDefault = () => 
+    <div>
+        <ul>
+            <li><Link to={routes.SIGN_IN}>Sign In</Link></li>
+            <li><Link to={routes.LANDING}>Landing</Link></li>
+        </ul>
+    </div>
+    
 export default Navigation;
