e74a1757f9eccbb5c1fd634413399c5d6ef56511
iustin <iustin.nitza@gmail.com>
2018-04-07 14:08:40 +0200

    Fix auth arrow functions

--- src/firebase/auth.js
+++ src/firebase/auth.js
@@ -5,21 +5,23 @@ export const doCreateUserWithEmailAndPassword = (email, password) =>
     auth.createUserWithEmailAndPassword(email, password);
 
 //Sing In
-export const doSignInWithEmailAndPassword = (email, password) => {
+export const doSignInWithEmailAndPassword = (email, password) => 
     auth.signInWithEmailAndPassword(email, password);
-}
 
 //Sing Out
-export const doSignOut = () => {
+export const doSignOut = () => 
     auth.signOut();
-}
 
 // Reset Password
-export const doResetPassword = (email) => {
+export const doResetPassword = (email) => 
     auth.sendPasswordResetEmail(email);
-}
 
 // Change Password
-export const doChangePassword = (newPassword) => {
+export const doChangePassword = (newPassword) => 
     auth.currentUser.updatePassword(newPassword);
-}
+
+export const doOnAuthStateChanged = () => 
+    auth.onAuthStateChanged();
+
+export const currentUser = () => 
+    auth.currentUser;
