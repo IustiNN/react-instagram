3f80b33f942b6b6b33713a402d887eb2306b9aaf
iustin <iustin.nitza@gmail.com>
2018-04-07 16:11:14 +0200

    Add forgot password page

--- src/components/user/ForgotPassword.js
+++ src/components/user/ForgotPassword.js
@@ -1,11 +1,94 @@
-import React from 'react'
+import React, { Component } from 'react';
+import {
+    withRouter,
+    Link
+} from 'react-router-dom';
+import { auth } from '../../firebase';
 
-const ForgotPasswordPage = () => {
+import * as routes from '../../constants/routes';
+
+const ForgotPasswordPage = ({ history }) => {
     return (
         <div>
-            Forgot password
+            <h1>Forgot Password</h1>
+            <ResetPasswordForm history={history}></ResetPasswordForm>
         </div>
-    )
+    );
+};
+
+const INITIAL_STATE = {
+    email: '',
+    error: null,
+};
+
+const byPropKey = (propertyName, value) => () => ({
+    [propertyName]: value,
+});
+
+class ResetPasswordForm extends Component {
+    constructor(props) {
+        super(props)
+        this.state = { ...INITIAL_STATE };
+    }
+
+    onSubmit = (event) => {
+        const {
+            email
+        } = this.state;
+
+        const {
+            history
+        } = this.props;
+        event.preventDefault();
+        auth.doResetPassword(email)
+            .then(() => {
+                // reset the form inputs and redirect to feed
+                this.setState(() => ({ ...INITIAL_STATE }));
+                console.log('We have sent you an email!');
+                history.push(routes.FEED);
+            }, (error) => {
+                this.setState(byPropKey('error', error));
+                console.log(error);
+            });
+    }
+
+    render() {
+        const {
+            email,
+            error,
+        } = this.state;
+
+        const isInvalid =
+            email === '';
+        return (
+            <form onSubmit={this.onSubmit}>
+                <input
+                    value={email}
+                    onChange={event => this.setState(byPropKey('email', event.target.value))}
+                    type="email"
+                    placeholder="Enter your email" />
+
+                <button type="submit" disabled={isInvalid}>Reset Password</button>
+
+                {error &&
+                    <div>
+                        <p>{error.message}</p>
+                    </div>
+                }
+
+            </form>
+        )
+    }
 }
 
-export default ForgotPasswordPage
+const PasswordForgetLink = () =>
+    <p>
+        <Link to={routes.FORGOT_PASSWORD}>Forgot Password?</Link>
+    </p>
+
+export {
+    ResetPasswordForm,
+    PasswordForgetLink
+}
+
+export default withRouter(ForgotPasswordPage);
