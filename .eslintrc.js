3b7c141140b932be2b24ecbad22eeae34873097e
iustin <iustin.nitza@gmail.com>
2018-04-25 19:30:07 +0200

    change eslint configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,22 @@
-module.exports = {
-    "extends": "standard",
-    "plugins": [
-        "standard",
-        "promise"
-    ]
-};
+{
+    "parser": "babel-eslint",
+    "env": {
+        "browser": true,
+        "node": true
+    },
+    "ecmaFeatures": {
+        "jsx": true,
+        "modules": true
+    },
+    "rules": {
+        "eol-last": 2,
+        "no-irregular-whitespace": 2,
+        "no-mixed-requires": 2,
+        "no-multi-spaces": 2,
+        "no-underscore-dangle": 0,
+        "quotes": [2, "single"],
+        "semi": [2, "never"],
+        "strict": 0,
+        "react/jsx-uses-vars": [2]
+    }
+}
